c2eb511f1f252175b9aaf1f3fc10d151337c5cae
Brian Toon <brian.elliot.toon@gmail.com>
2024-12-12 13:21:37 -0700

    clean up

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ let worstTeam = null;
 
 for (let team of teamList) {
   let teamStr = team.textContent;
-  let teamStrSplit = teamStr.split(":")
+  let teamStrSplit = teamStr.split(":");
   let teamName = teamStrSplit[0];
 
   let teamRecord = teamStrSplit[1];
-  let teamRecordArr = teamRecord.split(",")
+  let teamRecordArr = teamRecord.split(",");
 
   let teamWins = teamRecordArr[0];
-  let wins = teamWins.split(" ")[1]
-  let winsNumber = Number(wins)
+  let wins = teamWins.split(" ")[1];
+  let winsNumber = Number(wins);
 
   let teamLosses = teamRecordArr[1];
-  let losses = teamLosses.split(" ")[1]
-  let lossesNumber = Number(losses)
+  let losses = teamLosses.split(" ")[1];
+  let lossesNumber = Number(losses);
 
   let winningPercantage = Math.round((winsNumber / ( winsNumber + lossesNumber )) * 100);
 
@@ -36,7 +36,6 @@ for (let team of teamList) {
 }
 
 teams.forEach(team => {
-
   if (!bestTeam || bestTeam.winningPercantage < team.winningPercantage) {
     bestTeam = team;
   }
@@ -53,8 +52,8 @@ console.log(bestTeam);
 const bestTeamEl = document.createElement("p");
 const worstTeamEl = document.createElement("p");
 
-bestTeamEl.textContent = `The best team was ${bestTeam.teamName} with a winning percntage of ${bestTeam.winningPercantage}%.`
-worstTeamEl.textContent = `The worst team was ${worstTeam.teamName} with a winning percntage of ${worstTeam.winningPercantage}%.`
+bestTeamEl.textContent = `The best team was ${bestTeam.teamName} with a winning percntage of ${bestTeam.winningPercantage}%.`;
+worstTeamEl.textContent = `The worst team was ${worstTeam.teamName} with a winning percntage of ${worstTeam.winningPercantage}%.`;
 
 summary.appendChild(bestTeamEl);
 summary.appendChild(worstTeamEl);
